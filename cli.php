b8297bb615f6d0f22308f3b2a7e57fed81c9252f
Elenatr07 <e.turkina2010@yandex.ru>
2023-05-04 15:30:59 +1000

    Update cli.php

--- cli.php
+++ cli.php
@@ -4,9 +4,10 @@ use Geekbrains\LevelTwo\Blog\User;
 use Geekbrains\LevelTwo\Blog\UUID;
 use Geekbrains\LevelTwo\Person\Name;
 use Geekbrains\LevelTwo\Blog\Repositories\UsersRepository\SqliteUsersRepository;
+
 include __DIR__ . "/vendor/autoload.php";
 
-$connection = new PDO ('sqlite:' . __DIR__ . '/blog.sqlite');
+$connection = new PDO ('sqlite:' . __DIR__. '/blog.sqlite');
 $usersRepository = new SqliteUsersRepository($connection);
 
 $usersRepository->save(new User(UUID::random(), new Name('Ivan', 'Ivanov'), "admin"));
